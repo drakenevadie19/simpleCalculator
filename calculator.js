b4df5b46f37bd7c38180f33d7ffacba131c2d9cb
drakenevadie19 <drakennievadje19@gmail.com>
2024-04-26 08:32:20 -0500

    New Change: Adding function to 1-9 buttons

---
+++ calculator.js
@@ -0,0 +1,41 @@
+let currentResult = 0;
+
+function clickNine() {
+    currentResult = currentResult * 10 + 9;
+    console.log(currentResult)
+}
+
+function clickEight() {
+    currentResult = currentResult * 10 + 8;
+    console.log(currentResult)
+}
+
+function clickSeven() {
+    currentResult = currentResult * 10 + 7;
+    console.log(currentResult)
+}
+
+function clickSix() {
+    currentResult = currentResult * 10 + 6;
+    console.log(currentResult)
+}
+function clickFive() {
+    currentResult = currentResult * 10 + 5;
+    console.log(currentResult)
+}
+function clickFour() {
+    currentResult = currentResult * 10 + 4;
+    console.log(currentResult)
+}
+function clickThree() {
+    currentResult = currentResult * 10 + 3;
+    console.log(currentResult)
+}
+function clickTwo() {
+    currentResult = currentResult * 10 + 2;
+    console.log(currentResult)
+}
+function clickOne() {
+    currentResult = currentResult * 10 + 1;
+    console.log(currentResult)
+}
