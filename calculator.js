f7a6be776e690bafc9da52200397e12343f9d6ec
drakenevadie19 <drakennievadje19@gmail.com>
2024-04-27 02:46:30 -0500

    New Change: Created basic +-*/ functions

--- calculator.js
+++ calculator.js
@@ -1,41 +1,162 @@
-let currentResult = 0;
+let numberOne = 0;
+let operation = '+';
+let numberTwo = 0;
+let currentNumber = 1;
 
 function clickNine() {
-    currentResult = currentResult * 10 + 9;
-    console.log(currentResult)
+    if (currentNumber === 1) {
+        numberOne = numberOne * 10 + 9;
+        console.log(numberOne)
+    } else {
+        numberTwo = numberTwo * 10 + 9;
+        console.log(numberTwo)
+    }
 }
 
 function clickEight() {
-    currentResult = currentResult * 10 + 8;
-    console.log(currentResult)
+    if (currentNumber === 1) {
+        numberOne = numberOne * 10 + 8;
+        console.log(numberOne)
+    } else {
+        numberTwo = numberTwo * 10 + 8;
+        console.log(numberTwo)
+    }
 }
 
 function clickSeven() {
-    currentResult = currentResult * 10 + 7;
-    console.log(currentResult)
+    if (currentNumber === 1) {
+        numberOne = numberOne * 10 + 7;
+        console.log(numberOne)
+    } else {
+        numberTwo = numberTwo * 10 + 7;
+        console.log(numberTwo)
+    }
 }
 
 function clickSix() {
-    currentResult = currentResult * 10 + 6;
-    console.log(currentResult)
+    if (currentNumber === 1) {
+        numberOne = numberOne * 10 + 6;
+        console.log(numberOne)
+    } else {
+        numberTwo = numberTwo * 10 + 6;
+        console.log(numberTwo)
+    }
 }
+
 function clickFive() {
-    currentResult = currentResult * 10 + 5;
-    console.log(currentResult)
+    if (currentNumber === 1) {
+        numberOne = numberOne * 10 + 5;
+        console.log(numberOne)
+    } else {
+        numberTwo = numberTwo * 10 + 5;
+        console.log(numberTwo)
+    }
 }
+
 function clickFour() {
-    currentResult = currentResult * 10 + 4;
-    console.log(currentResult)
+    if (currentNumber === 1) {
+        numberOne = numberOne * 10 + 4;
+        console.log(numberOne)
+    } else {
+        numberTwo = numberTwo * 10 + 4;
+        console.log(numberTwo)
+    }
 }
+
 function clickThree() {
-    currentResult = currentResult * 10 + 3;
-    console.log(currentResult)
+    if (currentNumber === 1) {
+        numberOne = numberOne * 10 + 3;
+        console.log(numberOne)
+    } else {
+        numberTwo = numberTwo * 10 + 3;
+        console.log(numberTwo)
+    }
 }
+
 function clickTwo() {
-    currentResult = currentResult * 10 + 2;
-    console.log(currentResult)
+    if (currentNumber === 1) {
+        numberOne = numberOne * 10 + 2;
+        console.log(numberOne)
+    } else {
+        numberTwo = numberTwo * 10 + 2;
+        console.log(numberTwo)
+    }
 }
+
 function clickOne() {
-    currentResult = currentResult * 10 + 1;
-    console.log(currentResult)
+    if (currentNumber === 1) {
+        numberOne = numberOne * 10 + 1;
+        console.log(numberOne)
+    } else {
+        numberTwo = numberTwo * 10 + 1;
+        console.log(numberTwo)
+    }
+}
+
+function clickZero() {
+    if (currentNumber === 1) {
+        numberOne = numberOne * 10 + 0;
+        console.log(numberOne)
+    } else {
+        numberTwo = numberTwo * 10 + 0;
+        console.log(numberTwo)
+    }
+}
+
+function clickAddition() {
+    operation = "+";
+    if (currentNumber === 1) {
+        currentNumber = 2;
+    }
+}
+
+function clickSubstraction() {
+    operation = "-";
+    if (currentNumber === 1) {
+        currentNumber = 2;
+    }
+}
+
+function clickMultiplication() {
+    operation = "*";
+    if (currentNumber === 1) {
+        currentNumber = 2;
+    }
+}
+
+function clickDivision() {
+    operation = "/";
+    if (currentNumber === 1) {
+        currentNumber = 2;
+    }
+}
+
+function clickClear() {
+    numberOne = 0;
+    operation = '+';
+    numberTwo = 0;
+    currentNumber = 1;
+
+    console.log("Number 1: " + numberOne);
+    console.log("operation: " + operation);
+    console.log("Number 2: " + numberTwo);
+    console.log("currentNumber: " + currentNumber);
+}
+
+function calculate() {
+    if (operation === '+') {
+        console.log(numberOne + numberTwo);
+    }
+
+    if (operation === '-') {
+        console.log(numberOne - numberTwo);
+    }
+
+    if (operation === '*') {
+        console.log(numberOne * numberTwo);
+    }
+
+    if (operation === '/') {
+        console.log(numberOne / numberTwo);
+    }
 }
